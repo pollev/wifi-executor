ae30251d043da2c3ef639e325d6173882f750a7a
polle <vanhoofpolle@gmail.com>
2017-01-28 18:40:57 +0100

    added basic test command for detecting wifi passwords

---
+++ src/commands/DetectWifiPassCommand.java
@@ -0,0 +1,45 @@
+package commands;
+
+import shellcommand.Command;
+import shellcommand.CommandExecutor;
+import shellcommand.CommandExecutor.OS;
+
+/**
+ * A command class for detecting stored wifi passwords over multiple OS types.
+ * @author polle
+ *
+ */
+public class DetectWifiPassCommand extends Command{
+
+	// The command to be executed
+	private final String command;
+	private final OS osType;
+	
+	/**
+	 * Create the command for grabbing the wifi passwords from a computer
+	 * Autodetects the OS type and modifies commands appropriately.
+	 * 
+	 */
+	public DetectWifiPassCommand() {
+		this.osType = CommandExecutor.getCommandExecutor().getOSType();
+		
+		if(this.osType == OS.UNIX){
+			this.command = "echo not implemented";
+		}else if(this.osType == OS.WINDOWS){
+			this.command = "echo not implemented";
+		}else{
+			this.command = "echo OS type not supported";
+		}
+	}
+	
+	@Override
+	public String getCommand() {
+		return this.command;
+	}
+
+	@Override
+	public OS getOSType() {
+		return this.osType;
+	}
+
+}
--- src/extractor/Extractor.java
+++ src/extractor/Extractor.java
@@ -1,11 +1,26 @@
 package extractor;
 
+import java.io.IOException;
+
+import commands.DetectWifiPassCommand;
+import shellcommand.Command;
+import shellcommand.CommandExecutor;
+import shellcommand.NonMatchingOSException;
 
 public class Extractor {
 
 	public static void main(String[] args) {
 
+		CommandExecutor exec = CommandExecutor.getCommandExecutor();
+		Command detectWifiCommand = new DetectWifiPassCommand();
 		
+		try {
+			exec.executeCommand(detectWifiCommand);
+			System.out.println(detectWifiCommand.getNormalOutput());
+		} catch (NonMatchingOSException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
